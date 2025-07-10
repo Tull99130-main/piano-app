f057ce33d6e5fa44cd3630f99cb8f9ea748c5f5b
Your Name <your.email@example.com>
2025-07-10 16:51:38 -0400

    Your commit message describing the update

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useState } from "react";
+import ReactDOM from "react-dom/client";
+import MainMenu from "./components/MainMenu";
+import Piano from "./components/Piano"; // import your Piano component
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const App = () => {
+  const [showPiano, setShowPiano] = useState(false);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  return (
+    <>
+      {showPiano ? (
+        <Piano />
+      ) : (
+        <MainMenu onStart={() => setShowPiano(true)} />
+      )}
+    </>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
