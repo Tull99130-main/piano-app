a9b7c7395520ea3021d54620841acd65c430e641
Your Name <your.email@example.com>
2025-07-10 16:44:36 -0400

    Add missing initial React app files

---
+++ src/App.js
@@ -0,0 +1,19 @@
+
+import MainMenu from './components/MainMenu';
+import Piano from './components/Piano';
+
+function App() {
+  const [started, setStarted] = useState(false);
+
+  return (
+    <div>
+      {!started ? (
+        <MainMenu onStart={() => setStarted(true)} />
+      ) : (
+        <Piano />
+      )}
+    </div>
+  );
+}
+
+export default App;
